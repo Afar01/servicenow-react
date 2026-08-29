5e10f9e2de3bc4ec399c1f0d746f5188510a8571
Kedir Hassen <kedirhassen1@outlook.com>
2026-08-28 23:19:07 -0400

    Update redirectUri for Azure deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { PublicClientApplication, EventType } from "@azure/msal-browser";
+import { PublicClientApplication} from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./auth/msalConfig";
 import App from "./App";
